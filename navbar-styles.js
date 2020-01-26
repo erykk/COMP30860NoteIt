8782818b96f5965bb9737c087ed1c50b6d8fbe33
Gnxela <qawswedfrgtt@gmail.com>
2020-01-26 18:08:50 +0000

    Reformatted code. Replaced all spaces with tabs.

--- navbar-styles.js
+++ navbar-styles.js
@@ -1,12 +1,9 @@
-
 function alterNavBarSearch(x) {
-
-  if (x.classList.contains("navbar-search-inactive")) {
-    x.classList.add("navbar-search-active");
-    x.classList.remove("navbar-search-inactive");
-  }
-  else {
-    x.classList.add("navbar-search-inactive");
-    x.classList.remove("navbar-search-active");
-  }
+	if (x.classList.contains("navbar-search-inactive")) {
+		x.classList.add("navbar-search-active");
+		x.classList.remove("navbar-search-inactive");
+	} else {
+		x.classList.add("navbar-search-inactive");
+		x.classList.remove("navbar-search-active");
+	}
 }
