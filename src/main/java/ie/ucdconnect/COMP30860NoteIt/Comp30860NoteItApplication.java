48de257756dc735bb725ea8d686ff68739dfe69b
Gnxela <qawswedfrgtt@gmail.com>
2020-01-31 13:40:15 +0000

    Setup Spring Boot

---
+++ src/main/java/ie/ucdconnect/COMP30860NoteIt/Comp30860NoteItApplication.java
@@ -0,0 +1,13 @@
+package ie.ucdconnect.COMP30860NoteIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp30860NoteItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp30860NoteItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/ucdconnect/COMP30860NoteIt/Comp30860NoteItApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.ucdconnect.COMP30860NoteIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp30860NoteItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
